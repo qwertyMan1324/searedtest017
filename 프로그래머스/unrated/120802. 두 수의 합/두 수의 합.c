7c60f43e2ba29436d980db923cca8e0c569d92bb
qwertyMan1324 <148245329+qwertyMan1324@users.noreply.github.com>
2023-10-21 22:58:39 +0900

    [level unrated] Title: 두 수의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120802. 두 수의 합/두 수의 합.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) 
+{
+    return num1 + num2;
+}
+
+int main() {
+    int num1 = 0;
+    int num2 = 0;
+    int sum = solution(num1, num2);
+
+    return 0;
+}
