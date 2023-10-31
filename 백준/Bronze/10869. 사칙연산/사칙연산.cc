c58a3cd885e2faca7750d53f708d5bb4b7cb728f
qwertyMan1324 <148245329+qwertyMan1324@users.noreply.github.com>
2023-10-31 17:19:51 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	int p = a + b;
+	cout << p << endl;
+	int m = a - b;
+	cout << m << endl;
+	int s = a * b;
+	cout << s << endl;
+	int n = a / b;
+	cout << n << endl;
+	int l = a % b;
+	cout << l << endl;
+
+	return 0;
+
+}
