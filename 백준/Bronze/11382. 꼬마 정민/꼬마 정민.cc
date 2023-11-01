2ccd0b8c3110c114e437074aedec2da4fd869d41
qwertyMan1324 <148245329+qwertyMan1324@users.noreply.github.com>
2023-11-01 21:23:51 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{	
+	long long a, b, c;
+
+	cin >> a >> b >> c;
+
+	long long result = a + b + c;
+
+	cout << result;
+
+	return 0;
+
+}
