4c507ccb7153c4c86efc3fce6867d031be287f31
qwertyMan1324 <148245329+qwertyMan1324@users.noreply.github.com>
2023-11-01 21:41:11 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{	
+
+	cout << "\\""    " "/""\\" << endl;
+	cout << " "")""  ""("" ""'"")" << endl;
+	cout << "(""  ""/""  "")" << endl;
+	cout << " ""\\""(__)""|" << endl;
+
+	return 0;
+
+}
